350390e0cb12ab653514aabb284063854654d3df
amos <807864656@qq.com>
2018-10-01 12:26:27 +0800

    init project

---
+++ app/src/main/java/com/amos/myweather/MainActivity.java
@@ -0,0 +1,17 @@
+package com.amos.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+*created by amos on 10.1.18
+*/
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
