ad74df8bd61eb79dcfb23a3d03f0a30ef0046925
amos <807864656@qq.com>
2018-10-09 11:29:03 +0800

    NetCheck

--- app/src/main/java/com/amos/myweather/MainActivity.java
+++ app/src/main/java/com/amos/myweather/MainActivity.java
@@ -2,28 +2,29 @@ package com.amos.myweather;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.amos.util.NetUtil;
 
 
 /**
 *created by amos on 10.1.18
 */
 
-public class MainActivity extends Activity implements View.OnClickListener{
-    private Button button1;
+public class MainActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
-        button1=(Button)findViewById(R.id.button1);
-        button1.setOnClickListener(this);
-    }
-    @Override
-    public void onClick(View view){
-        switch (view.getId()){
-            case R.id.button1:Toast.makeText(MainActivity.this,"Hello!",Toast.LENGTH_SHORT).show();
+
+        if(NetUtil.getNetworkState(this)!=NetUtil.NETWORK_NONE){
+            Log.d("MyWeather","网络OK");
+            Toast.makeText(MainActivity.this, "网络OK", Toast.LENGTH_LONG).show();
+        }else{
+            Log.d("MyWeather","网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了", Toast.LENGTH_LONG).show();
         }
+
     }
 }
